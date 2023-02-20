af4c41a2c7b999d34fa179f683b33a109bb6485e
INS-2022 <125819492+INS-2022@users.noreply.github.com>
2023-02-20 19:46:36 +0530

    Update Q41.js

--- WTL/Q41/Q41.js
+++ WTL/Q41/Q41.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 const serveVideo = (req, resp) => {
   resp.writeHead(200, { "content-Type": "video/mp4" });
-  var video = fs.createReadStream("sky.mp4");
+  var video = fs.createReadStream("https://ik.imagekit.io/PaxyifierProjTest32/Untitled_cYpP2Bvja.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1676902575072");
   video.pipe(resp);
   // resp.end();
 };
