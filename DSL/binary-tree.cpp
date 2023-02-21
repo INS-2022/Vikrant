5502f7e604b2abce2594a819bac1ecef13149be9
INS-2022 <125819492+INS-2022@users.noreply.github.com>
2023-02-21 13:44:20 +0000

    9-14 done

---
+++ DSL/binary-tree.cpp
@@ -0,0 +1,329 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+class BinarySearchTree{
+    private:
+        Node *root;
+    public:
+        BinarySearchTree(){
+            root = NULL;
+        }
+        Node* getRoot(){
+            return root;
+        }
+        void insert(int data){
+            insert(root, data);
+        }
+        void insert(Node *node, int data){
+            if(root == NULL){
+                root = new Node;
+                root->data = data;
+                root->left = NULL;
+                root->right = NULL;
+            }else{
+                if(data < node->data){
+                    if(node->left != NULL){
+                        insert(node->left, data);
+                    }else{
+                        node->left = new Node;
+                        node->left->data = data;
+                        node->left->left = NULL;
+                        node->left->right = NULL;
+                    }
+                }else if(data >= node->data){
+                    if(node->right != NULL){
+                        insert(node->right, data);
+                    }else{
+                        node->right = new Node;
+                        node->right->data = data;
+                        node->right->left = NULL;
+                        node->right->right = NULL;
+                    }
+                }
+            }
+        }
+        void remove(int data){
+            remove(root, data);
+        }
+        Node* remove(Node *node, int data){
+            if(node == NULL){
+                return node;
+            }else if(data < node->data){
+                node->left = remove(node->left, data);
+            }else if(data > node->data){
+                node->right = remove(node->right, data);
+            }else{
+                if(node->left == NULL && node->right == NULL){
+                    delete node;
+                    node = NULL;
+                }else if(node->left == NULL){
+                    Node *temp = node;
+                    node = node->right;
+                    delete temp;
+                }else if(node->right == NULL){
+                    Node *temp = node;
+                    node = node->left;
+                    delete temp;
+                }else{
+                    Node *temp = findMin(node->right);
+                    node->data = temp->data;
+                    node->right = remove(node->right, temp->data);
+                }
+            }
+            return node;
+        }
+        void display(){
+            display(root);
+            cout << endl;
+        }
+        void display(Node *node){
+            if(node != NULL){
+                display(node->left);
+                cout << node->data << " ";
+                display(node->right);
+            }
+        }
+        void preOrder(){
+            preOrder(root);
+            cout << endl;
+        }
+        void preOrder(Node *node){
+            if(node != NULL){
+                cout << node->data << " ";
+                preOrder(node->left);
+                preOrder(node->right);
+            }
+        }
+        void inOrder(){
+            inOrder(root);
+            cout << endl;
+        }
+        void inOrder(Node *node){
+            if(node != NULL){
+                inOrder(node->left);
+                cout << node->data << " ";
+                inOrder(node->right);
+            }
+        }
+        void postOrder(){
+            postOrder(root);
+            cout << endl;
+        }
+        void postOrder(Node *node){
+            if(node != NULL){
+                postOrder(node->left);
+                postOrder(node->right);
+                cout << node->data << " ";
+            }
+        }
+        Node* search(int data){
+            return search(root, data);
+        }
+        Node* search(Node *node, int data){
+            if(node != NULL){
+                if(node->data == data){
+                    return node;
+                }else if(data < node->data){
+                    return search(node->left, data);
+                }else{
+                    return search(node->right, data);
+                }
+            }else{
+                return NULL;
+            }
+        }
+        Node* findMin(){
+            return findMin(root);
+        }
+        Node* findMin(Node *node){
+            if(node == NULL){
+                return NULL;
+            }else if(node->left == NULL){
+                return node;
+            }else{
+                return findMin(node->left);
+            }
+        }
+        Node* findMax(){
+            return findMax(root);
+        }
+        Node* findMax(Node *node){
+            if(node == NULL){
+                return NULL;
+            }else if(node->right == NULL){
+                return node;
+            }else{
+                return findMax(node->right);
+            }
+        }
+        int height(){
+            return height(root);
+        }
+        int height(Node *node){
+            if(node == NULL){
+                return -1;
+            }else{
+                int leftHeight = height(node->left);
+                int rightHeight = height(node->right);
+                if(leftHeight > rightHeight){
+                    return leftHeight + 1;
+                }else{
+                    return rightHeight + 1;
+                }
+            }
+        }
+        int size (){
+            return size(root);
+        }
+        int size(Node *node){
+            if(node == NULL){
+                return 0;
+            }else{
+                return size(node->left) + 1 + size(node->right);
+            }
+        }
+        int countNodes(){
+            return countNodes(root);
+        }
+        int countNodes(Node *node){
+            if(node == NULL || (node->left == NULL && node->right == NULL)){
+                return 0;
+            }else if(node->left != NULL && node->right != NULL){
+                return 1 + countNodes(node->left) + countNodes(node->right);
+            }else{
+                return countNodes(node->left) + countNodes(node->right);
+            }
+        }
+
+};
+int main(){
+    BinarySearchTree bst = BinarySearchTree();
+    bool run = true;
+    // test cases
+    if (!run){
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(1);
+        bst.insert(4);
+        bst.insert(7);
+        bst.insert(6);
+        bst.insert(8);
+        bst.display();
+        cout << "countNodes: "<<bst.countNodes() << endl;
+        cout << "size: "<<bst.size() << endl;
+        cout << "height: "<<bst.height() << endl;
+        cout << "findMin: "<<bst.findMin()->data << endl;
+        cout << "findMax: "<<bst.findMax()->data << endl;
+        cout << "search: "<<bst.search(8)->data << endl;
+        cout << "search: "<<bst.search(9)->data << endl;
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(1);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(3);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(5);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(4);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(6);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(7);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.postOrder();
+        bst.remove(8);
+        bst.display();
+        bst.preOrder();
+        bst.inOrder();
+        bst.display();
+        bst.postOrder();
+        run = true;
+
+    }
+    // Program
+    int choice, data;
+    while (run){
+        cout << "1. Insert" << endl;
+        cout << "2. Remove" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. PreOrder" << endl;
+        cout << "5. InOrder" << endl;
+        cout << "6. PostOrder" << endl;
+        cout << "7. Search" << endl;
+        cout << "8. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice){
+            case 1:
+                cout << "Enter data: ";
+                cin >> data;
+                bst.insert(data);
+                break;
+            case 2:
+                cout << "Enter data: ";
+                cin >> data;
+                bst.remove(data);
+                break;
+            case 3:
+                bst.display();
+                cout << endl;
+                break;
+            case 4:
+                bst.preOrder();
+                cout << endl;
+                break;
+            case 5:
+                bst.inOrder();
+                cout << endl;
+                break;
+            case 6:
+                bst.postOrder();
+                cout << endl;
+                break;
+            case 7:
+                cout << "Enter data: ";
+                cin >> data;
+                if(bst.search(data) != NULL){
+                    cout << "Found" << endl;
+                }else{
+                    cout << "Not Found" << endl;
+                }
+                break;
+            case 8:
+                run = false;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
+                break;
+        }
+
+    }
+
+
+    return 0;
+}
---
+++ DSL/graph-bfs.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data,Node *next=NULL){
+        this->data = data;
+        if (next != NULL){
+            this->next = next;
+        }
+    }
+};
+class Queue{
+    public:
+    Node *front;
+    Node *rear;
+    Queue(){
+        front = NULL;
+        rear = NULL;
+    }
+    void enqueue(int data){
+        if (front == NULL){
+            front = new Node(data);
+            rear = front;
+        }
+        else{
+            rear->next = new Node(data);
+            rear = rear->next;
+        }
+    }
+    int dequeue(){
+        if (front == NULL){
+            return -1;
+        }
+        else{
+            int temp = front->data;
+            front = front->next;
+            return temp;
+        }
+    }
+    void print(){
+        Node *temp = front;
+        while(temp != NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+};
+class Term{
+    public:
+    int node1,node2,value=1;
+    Term *next;
+    Term(int node1,int node2,Term *next=NULL){
+        this->node1 = node1;
+        this->node2 = node2;
+        if (next != NULL){
+            this->next = next;
+        }
+    }
+};
+class Graph {
+    public:
+    Term *head;
+    int nodes;
+    Graph(int nodes){
+        this->nodes = nodes;
+        head = NULL;
+    }
+    void addTerm(int node1,int node2){
+        if (head == NULL){
+            head = new Term(node1,node2);
+        }
+        else{
+            Term *temp = head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = new Term(node1,node2);
+        }
+    }
+    void print(){
+        Term *temp = head;
+        for (int i=1; i<=nodes; i++) {
+            for (int j=1; j<=nodes; j++) {
+                if (temp == NULL) {
+                    cout<<"0 ";
+                    continue;
+                }
+                if (temp->node1 == i && temp->node2 == j) {
+                    cout<<temp->value<<" ";
+                    temp = temp->next;
+                }
+                else {
+                    cout<<"0 ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+    int *bfs(int node){
+        int *visited = new int[nodes+1];
+        for (int i=1; i<nodes+1; i++) {
+            visited[i] = 0;
+        }
+        visited[node] = 1;
+        Queue *queue = new Queue();
+        queue->enqueue(node);
+        cout << "Queue :: Enqueued "<<node<<": ";
+        queue->print();
+        while (queue->front != NULL) {
+            int temp = queue->dequeue();
+            cout << "Queue :: Dequeued "<<temp<<": ";
+            queue->print();
+            Term *tempTerm = head;
+            while (tempTerm != NULL) {
+                if (tempTerm->node1 == temp && visited[tempTerm->node2] == 0) {
+                    queue->enqueue(tempTerm->node2);
+                    cout << "Queue :: Enqueued "<<tempTerm->node2<<": ";
+                    queue->print();
+                    // printArray(visited, 0, nodes-1);
+                    visited[tempTerm->node2] = 1;
+                }
+                tempTerm = tempTerm->next;
+            }
+        }
+        return visited;
+    }
+    void printArray(int *arr, int start, int end){
+        for (int i=start; i<=end; i++) {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main() {
+    bool test = false;
+    if (test){
+        // delete the if block
+        Graph *graph = new Graph(7);
+        graph->addTerm(1,2);
+        graph->addTerm(1,4);
+        graph->addTerm(2,1);
+        graph->addTerm(2,3);
+        graph->addTerm(2,5);
+        graph->addTerm(3,5);
+        graph->addTerm(3,6);
+        graph->addTerm(3,7);
+        graph->addTerm(4,1);
+        graph->addTerm(4,5);
+        graph->addTerm(5,2);
+        graph->addTerm(5,4);
+        graph->addTerm(5,6);
+        graph->addTerm(6,3);
+        graph->addTerm(6,5);
+        graph->addTerm(6,7);
+        graph->addTerm(7,3);
+        graph->addTerm(7,6);
+        //print graph
+        graph->print();
+        //bfs
+        cout<<"bfs:"<<endl;
+        int *visited = graph->bfs(1);
+        graph->printArray(visited, 1, 7);
+        exit(0);
+    }
+    cout << "Graph" << endl;
+    cout << "Enter number of Nodes in the graph: ";
+    int nodes;
+    cin >> nodes;
+    Graph s = Graph(nodes);
+    cout << "Enter number of edges: ";
+    int edges;
+    cin >> edges;
+    for (int i=0; i<edges; i++) {
+        cout << "Enter node1: ";
+        int node1;
+        cin >> node1;
+        cout << "Enter node2: ";
+        int node2;
+        cin >> node2;
+        s.addTerm(node1,node2);
+    }
+    s.print();
+    int *visited = s.bfs(2);
+    cout << "BFS: ";
+    s.printArray(visited, 1, nodes);
+    return 0;
+}
